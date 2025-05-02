3c20563bfde3d261d697469042b55c853c8eda34
Anas <anas.ibrahimi@gmail.com>
2025-05-02 19:58:05 +0100

    updating principale view

--- src/main/java/com/anas/portfolio/service/ContactService.java
+++ src/main/java/com/anas/portfolio/service/ContactService.java
@@ -10,26 +10,26 @@ import java.util.Optional;
 
 @Service
 public class ContactService {
-    
+
     @Autowired
     private ContactRepository contactRepository;
-    
+
     public Contact saveContact(Contact contact) {
         return contactRepository.save(contact);
     }
-    
+
     public List<Contact> getAllContacts() {
         return contactRepository.findAllByOrderByCreatedAtDesc();
     }
-    
+
     public List<Contact> getUnreadContacts() {
         return contactRepository.findByReadOrderByCreatedAtDesc(false);
     }
-    
+
     public Optional<Contact> getContactById(Long id) {
         return contactRepository.findById(id);
     }
-    
+
     public Contact markAsRead(Long id) {
         Optional<Contact> contactOpt = contactRepository.findById(id);
         if (contactOpt.isPresent()) {
@@ -39,11 +39,11 @@ public class ContactService {
         }
         return null;
     }
-    
+
     public void deleteContact(Long id) {
         contactRepository.deleteById(id);
     }
-    
+
     public long getUnreadCount() {
         return contactRepository.countByRead(false);
     }
